50afaf7b9a4d7f17fa87dac87c191d3daa8a7df5
ersin <ersin-kaya@outlook.com.tr>
2024-12-02 11:49:17 +0300

    Updated render logic to handle list ids

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const listsContainer = document.querySelector("[data-lists]");
 
-let lists = ["name", "todo"];
+let lists = [
+  { id: 1, name: "name" },
+  { id: 2, name: "todo" },
+];
 
 function render() {
   clearElement(listsContainer);
   lists.forEach((list) => {
     const listElement = document.createElement("li");
+    listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
-    listElement.innerText = list;
+    listElement.innerText = list.name;
     listsContainer.appendChild(listElement);
   });
 }
